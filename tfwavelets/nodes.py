ee67c41ee2f40829b33687532815bbb237407640
Vansh <96469228+TekhDude1@users.noreply.github.com>
2023-04-21 13:53:34 +0530

    Update nodes.py

--- tfwavelets/nodes.py
+++ tfwavelets/nodes.py
@@ -85,7 +85,7 @@ def cyclic_conv1d_alt(input_node, filter_):
     print(input_new.shape)
 
     # Convolve with periodic extension
-    result = tf.nn.conv1d(input_new, kernel_node[::-1], stride=1, padding="VALID")
+    result = tf.nn.conv2d(input_new, kernel_node[::-1], stride=1, padding="VALID")
 
     return result
 
