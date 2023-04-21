adcab76028c5dfbb6f1ed3d06c095c34ce1ae931
Vansh <96469228+TekhDude1@users.noreply.github.com>
2023-04-21 13:44:57 +0530

    Update dwtcoeffs.py

--- tfwavelets/dwtcoeffs.py
+++ tfwavelets/dwtcoeffs.py
@@ -18,7 +18,10 @@ Examples:
 """
 
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+print("disabled v2 1")
 from tfwavelets.utils import adapt_filter, to_tf_mat
 
 
